54ae803922741daeec64fcce92b5d6414fdfd497
Yashansh Rastogi <145865277+lets-code404@users.noreply.github.com>
2025-01-11 12:55:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ image = None
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
 # Button to submit the uploaded document
 submit = st.button("Submit")
